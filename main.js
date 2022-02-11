6e01ed0405ed30732bfa993b3a2c1c8a6697747b
Brian <blpmurphy@gmail.com>
2022-02-11 12:58:00 +0000

    Add drawHead and drawTail function for game

--- main.js
+++ main.js
@@ -14,9 +14,44 @@ canvas.height = 500;
 canvas.width = 1000;
 
 let apples = [];
-let snake = 
+
+let snake = {
+  // Snake head dimensions
+  headX: 100,
+  headY: 75,
+  headRadius: 15,
+  headColour: `#C46200`,
+  // Snake tail dimensions
+  tailX: 75,
+  tailY: 75,
+  tailRadius: 12,
+  tailColour: `#964B00`,
+};
 // Generate a random number between to values, inclusive
 let getRandomNum = function (min, max) {
   let randomNum = Math.floor(Math.random() * (max - min + 1) + min);
   return randomNum;
 };
+
+// Draw head of the snake
+let drawHead = function () {
+  ctx.beginPath();
+  ctx.arc(snake.headX, snake.headY, snake.headRadius, 0, 2 * Math.PI);
+  ctx.strokeStyle = snake.headColour;
+  ctx.stroke();
+  ctx.fillStyle = snake.headColour;
+  ctx.fill();
+};
+
+// Draw head of the snake
+let drawTail = function () {
+  ctx.beginPath();
+  ctx.arc(snake.tailX, snake.tailY, snake.tailRadius, 0, 2 * Math.PI);
+  ctx.strokeStyle = snake.tailColour;
+  ctx.stroke();
+  ctx.fillStyle = snake.tailColour;
+  ctx.fill();
+};
+
+drawHead();
+drawTail();
