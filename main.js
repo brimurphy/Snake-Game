27dd04138e6408613d8957fe4c0c557675bc9fd1
Brian <blpmurphy@gmail.com>
2022-02-11 12:08:01 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// To-do list
+// Start game function
+// Movement function
+// speed function
+// eat function
+// score function
+// crash/end game function
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+canvas.height = 500;
+canvas.width = 1000;
